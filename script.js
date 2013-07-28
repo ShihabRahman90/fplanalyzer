40793a9cce995e269912f2fd020ebde7c0fd1d34
esalman <cpu.salman@gmail.com>
2013-07-29 02:39:30 +0600

    handled team name ambiguity and empty fixture

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 window.fplAnalyzeTrigger = function () {
-    fixArr = {};
-    $.each($(".ismFixtureTable tbody tr"), function (e, t) {
-        fixArr[$(t).children(".ismHomeTeam").html()] = (fixArr[$(t).children(".ismHomeTeam").html()] == undefined ? "" : fixArr[$(t).children(".ismHomeTeam").html()].substring(0, 3) + ",") + $(t).children(".ismAwayTeam").html().toUpperCase().substring(0, 3);
-        fixArr[$(t).children(".ismAwayTeam").html()] = (fixArr[$(t).children(".ismAwayTeam").html()] == undefined ? "" : fixArr[$(t).children(".ismAwayTeam").html()].substring(0, 3) + ",") + $(t).children(".ismHomeTeam").html().toLowerCase().substring(0, 3)
-    });
-    $($("#ismTeamDisplayGraphical div[id^=ismGraphical]")[0]).find(".ismShirt").attr("title");
-    $.each($("#ismTeamDisplayGraphical div[id^=ismGraphical]"), function (e, t) {
-        $(t).find(".ismElementDetail dd").html(fixArr[$(t).find(".ismShirt").attr("title")].replace(" ", ""))
+	var teamArr = {
+		"Fulham":"FUL",
+		"Arsenal":"ARS",
+		"Everton":"EVE",
+		"West Brom":"WBA",
+		"Hull City":"HUL",
+		"Norwich":"NOR",
+		"Newcastle":"NEW",
+		"West Ham":"WHM",
+		"Southampton":"SOU",
+		"Sunderland":"SUN",
+		"Stoke City":"STK",
+		"Crystal Palace":"CPA",
+		"Aston Villa":"AVL",
+		"Liverpool":"LIV",
+		"Cardiff City":"CAR",
+		"Man City":"MCY",
+		"Tottenham":"TOT",
+		"Swansea":"SWA",
+		"Man Utd":"MUN",
+		"Chelsea":"CHE"
+	}
+    var fixArr = {}
+    $.each($(".ismFixtureTable tbody tr"), function (i, team) {
+        fixArr[$(team).children(".ismHomeTeam").html()] = (fixArr[$(team).children(".ismHomeTeam").html()] == undefined ? "" : fixArr[$(team).children(".ismHomeTeam").html()] + ",") + teamArr[$(team).children(".ismAwayTeam").html()].toUpperCase()
+        fixArr[$(team).children(".ismAwayTeam").html()] = (fixArr[$(team).children(".ismAwayTeam").html()] == undefined ? "" : fixArr[$(team).children(".ismAwayTeam").html()] + ",") + teamArr[$(team).children(".ismHomeTeam").html()].toLowerCase()
+    })
+    $($("#ismTeamDisplayGraphical div[id^=ismGraphical]")[0]).find(".ismShirt").attr("title")
+    $.each($("#ismTeamDisplayGraphical div[id^=ismGraphical]"), function (i, player) {
+        $(player).find(".ismElementDetail dd").html( fixArr[$(player).find(".ismShirt").attr("title")] == undefined ? "" : fixArr[$(player).find(".ismShirt").attr("title")] )
     })
 }
